f43a8f55a28f6be322280a4bcddbce247e50114f
alvinwikblad <Alvin.wikblad@gmail.com>
2024-03-07 12:56:10 +0100

    lol

--- src/labb5/MiniRäknare.java
+++ src/labb5/MiniRäknare.java
@@ -2,7 +2,13 @@ package labb5;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 //Alvin och Ammar
+/**
+ * klassen agerar som huvudprogammet.
+ */
 public class MiniRäknare {
+	/**
+	 * skaper en instans av GUI:n
+	 */
 	public static void main(String[] args) {
 		GUI MiniRäknare = new GUI(new Situation(new JLabel()));
 	}
--- src/labb5/Situation.java
+++ src/labb5/Situation.java
@@ -1,13 +1,17 @@
 package labb5;
 //Alvin och Ammar
 import javax.swing.JLabel;
-
+/**
+ * Klassen situation uppdaterar vilen stae som ska vara aktiv
+ */
 public class Situation {
 	 State state = State.Input1;
 	 JLabel display;
 	 BinOpButton binaryOperator;
 	 int leftOperand;
-	
+	/**
+	 * konstruktorn sätter vilken vilken display som motsvarar state
+	 */
 	Situation(JLabel display) {
 		this.display = display;
 	}
